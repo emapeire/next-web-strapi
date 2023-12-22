22da9609f283e36ea12c0105bc5285ee2c3d0437
Emanuel Peire <emapeire@outlook.com>
2023-12-22 16:12:30 -0300

    Add image domain to next.config.js and refactor getCoverImage function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import { getGames } from './services/videogames'
+import { getCoverImage, getGames } from './services/videogames'
 import { Datum } from './types.d'
 
 export default async function Home() {
@@ -8,9 +8,9 @@ export default async function Home() {
 
   return (
     <main className='flex min-h-screen flex-col items-center p-24'>
-      {games.map((game) => (
+      {games.map(({ attributes, id }) => (
         <Link
-          key={game.id}
+          key={id}
           href='#'
           className='flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'
         >
@@ -18,15 +18,15 @@ export default async function Home() {
             width={300}
             height={300}
             className='object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg'
-            src={game.attributes.cover.data.attributes.url}
-            alt={game.attributes.title}
+            src={getCoverImage(attributes)}
+            alt={attributes.title}
           />
           <div className='flex flex-col justify-between p-4 leading-normal'>
             <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
-              {game.attributes.title}
+              {attributes.title}
             </h5>
             <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>
-              {game.attributes.description[0].children[0].text}
+              {attributes.description[0].children[0].text}
             </p>
           </div>
         </Link>
--- src/app/services/videogames.ts
+++ src/app/services/videogames.ts
@@ -1,4 +1,5 @@
 import { API_URL } from '../config'
+import { DatumAttributes } from '../types.d'
 
 export async function getGames() {
   const res = await fetch(
@@ -9,4 +10,9 @@ export async function getGames() {
   }
   const { data } = await res.json()
   return data
+}
+
+export function getCoverImage(attributes: DatumAttributes) {
+  const { url } = attributes.cover.data.attributes
+  return `${API_URL}${url}`
 }
