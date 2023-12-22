325a3dc7efb8fcdb6c794ba472460eb19ce36608
Emanuel Peire <emapeire@outlook.com>
2023-12-22 14:07:09 -0300

    Add types

---
+++ src/app/types.d.ts
@@ -0,0 +1,54 @@
+export interface APIResults {
+  data: Datum[]
+  meta: Meta
+}
+
+export interface Datum {
+  id: number
+  attributes: DatumAttributes
+}
+
+export interface DatumAttributes {
+  title: string
+  launch: null
+  description: Description[]
+  slug: string
+  createdAt: Date
+  updatedAt: Date
+  publishedAt: Date
+  cover: Cover
+}
+
+export interface Cover {
+  data: Data
+}
+
+export interface Data {
+  id: number
+  attributes: DataAttributes
+}
+
+export interface DataAttributes {
+  url: string
+}
+
+export interface Description {
+  type: string
+  children: Child[]
+}
+
+export interface Child {
+  type: string
+  text: string
+}
+
+export interface Meta {
+  pagination: Pagination
+}
+
+export interface Pagination {
+  page: number
+  pageSize: number
+  pageCount: number
+  total: number
+}
