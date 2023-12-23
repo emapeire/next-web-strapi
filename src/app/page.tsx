2afd26ee14840e649f5a304bb294302001b59a41
Emanuel Peire <emapeire@outlook.com>
2023-12-23 15:18:22 -0300

    Refactor page layout and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,31 +15,33 @@ export default async function Home({ searchParams }: searchParamsProps) {
 
   return (
     <main className='flex min-h-screen flex-col items-center p-24'>
-      {data.map(({ id, cover, title, description }) => (
-        <Link
-          key={id}
-          href='#'
-          className='flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mb-8'
-        >
-          <Image
-            priority
-            width={250}
-            height={250}
-            className='object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg'
-            src={cover}
-            alt={title}
-          />
-          <div className='flex flex-col justify-between p-4 leading-normal'>
-            <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
-              {title}
-            </h5>
-            <div
-              className='mb-3 font-normal text-gray-700 dark:text-gray-400'
-              dangerouslySetInnerHTML={{ __html: description }}
+      <section className='min-h-80'>
+        {data.map(({ id, cover, title, description }) => (
+          <Link
+            key={id}
+            href='#'
+            className='flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'
+          >
+            <Image
+              priority
+              width={250}
+              height={250}
+              className='object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg'
+              src={cover}
+              alt={title}
             />
-          </div>
-        </Link>
-      ))}
+            <div className='flex flex-col justify-between p-4 leading-normal'>
+              <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+                {title}
+              </h5>
+              <div
+                className='mb-3 font-normal text-gray-700 dark:text-gray-400'
+                dangerouslySetInnerHTML={{ __html: description }}
+              />
+            </div>
+          </Link>
+        ))}
+      </section>
       <PaginationData pagination={pagination} />
     </main>
   )
