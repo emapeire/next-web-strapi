916d2aa86f31f1c3f066c91a1100b1dc031b3700
Emanuel Peire <emapeire@outlook.com>
2023-12-23 14:11:02 -0300

    Fix stroke attributes in Pagination component

--- src/app/components/Pagination.tsx
+++ src/app/components/Pagination.tsx
@@ -33,9 +33,9 @@ export function Pagination({ pagination }: Meta) {
           >
             <path
               stroke='currentColor'
-              stroke-linecap='round'
-              stroke-linejoin='round'
-              stroke-width='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
               d='M13 5H1m0 0 4 4M1 5l4-4'
             />
           </svg>
@@ -52,9 +52,9 @@ export function Pagination({ pagination }: Meta) {
           >
             <path
               stroke='currentColor'
-              stroke-linecap='round'
-              stroke-linejoin='round'
-              stroke-width='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
               d='M1 5h12m0 0L9 1m4 4L9 9'
             />
           </svg>
